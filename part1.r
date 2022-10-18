b548d517b40634908240c429a193c95cfc7bf114
laynejohnson13 <laynejohnson13001@gmail.com>
2022-10-18 15:14:22 -0400

    1.3/1.4 completed

--- part1.r
+++ part1.r
@@ -71,3 +71,85 @@ list1
 ###Exercise - create list
 list2 <- list(species, glengths, number)
 list2
+
+
+
+#####Example 3
+
+glengths <- c(glengths, 90) # adding at the end	
+glengths <- c(30, glengths) # adding at the beginning
+
+sqrt(81)
+
+sqrt(glengths)
+
+round(3.14159)
+
+##seeking help for functions
+
+?round
+
+args(round)
+
+example("round")
+
+round(3.14159, digits=2)
+##OR
+round(3.14159, 2)
+
+
+###Exercise - calculate mean value of glengths
+mean(glengths)
+
+test <- c(1, NA, 2, 3, NA, 4)
+test
+
+mean(test, na.rm = TRUE)
+
+##sort in ascending order
+sort(glengths)
+
+##sort is descending order
+sort(-glengths)
+
+
+square_it <- function(x) {
+  square <- x * x
+  return(square)
+}
+
+square_it(5)
+
+square_it <- function(x) {
+  x * x
+}
+
+###Exercise 
+multiply_it <- function(x,y) {
+  x * y
+}
+
+
+
+###Example
+metadata <- read.csv(file="data/mouse_exp_design.txt")
+metadata
+
+proj_summary <- read.table(file= 'data/project-summary.txt') 
+proj_summary
+
+##inspecting dataframes
+head(metadata)
+
+###Example
+class(glengths)
+class(metadata)
+
+summary(proj_summary)
+
+colnames(proj_summary)
+rownames(proj_summary)
+
+rownames(metadata)
+
+length(proj_summary)
